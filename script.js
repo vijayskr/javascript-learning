8c3408aa47d68ca25041896266eb30603618d8f0
Vijayasankar Balasubramanian <vijayasankar@outlook.com>
2018-07-09 21:00:53 -0500

    Variables

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-var firstName = "Vijayasankar";
+var markHeight, johnHeight;
 
-console.log(typeof(firstName));
+var markWeight, johnWeight;
 
-var undefined = null;
+markHeight = prompt("Enter Mark's Height");
+johnHeight = prompt("Enter John's Height");
 
-console.log(typeof(undefined));
+markWeight = prompt("Enter Mark's Weight");
+johnWeight = prompt("Enter John's Weight");
+
+var markBMI, johnBMI;
+
+markBMI = markWeight / (markHeight * markHeight);
+johnBMI = johnWeight / (johnHeight * johnHeight);
+
+if(markBMI > johnBMI)
+{
+    alert("Is Mark's BMI greater than John's?, TRUE");
+}
+else
+{
+    alert("Is Mark's BMI greater than John's?, FALSE");
+}
